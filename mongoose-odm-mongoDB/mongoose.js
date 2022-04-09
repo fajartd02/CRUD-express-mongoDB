e7bf6b04ae60f3f3d122c7f1c7f60acb7124eb7b
unknown <fajarmh2@gmail.com>
2022-04-09 16:05:31 +0700

    Update specific

--- mongoose-odm-mongoDB/mongoose.js
+++ mongoose-odm-mongoDB/mongoose.js
@@ -26,8 +26,15 @@ async function main() {
     // console.log(cars);
 
     // Get Specific ID ALL
-    const cars = await Car.find({_id: '6251474999500ff0c16be759'});
-    console.log(cars);
+    // const cars = await Car.find({_id: '6251474999500ff0c16be759'});
+    // console.log(cars);
+
+    // Update specific
+    // const updateCar = await Car.updateOne({_id: '6251485f97bbd77777261a52'}, {
+    //     name: 'Alphard'
+    // })
+
+    // console.log(updateCar);
 }
 
 
