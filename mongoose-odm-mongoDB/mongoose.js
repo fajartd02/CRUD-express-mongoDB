3f6f9c7d702d62cca254bfd31167d2ef667e409c
unknown <fajarmh2@gmail.com>
2022-04-09 16:09:07 +0700

    Delete and required schema

--- mongoose-odm-mongoDB/mongoose.js
+++ mongoose-odm-mongoDB/mongoose.js
@@ -3,7 +3,11 @@ const mongoose = require('mongoose');
 main().catch(err => console.log(err));
 
 const carSchema = new mongoose.Schema({
-    name: String
+    // Namenya wajib isi(ga bisa dikosongin)
+    name: {
+        type: String,
+        required: [true, "Nama gak boleh kosong"]
+    }
 });
 
 const Car = mongoose.model('Car', carSchema);
@@ -12,7 +16,7 @@ async function main() {
     await mongoose.connect('mongodb://localhost:27017/userDB');
 
     // Create
-    // const car = new Car({name: "Lamborghini"});
+    // const car = new Car({name: "Bugathi"});
     // car.save((err, result) => {
     //     if(err) {
     //         return console.log(err);
@@ -29,12 +33,15 @@ async function main() {
     // const cars = await Car.find({_id: '6251474999500ff0c16be759'});
     // console.log(cars);
 
-    // Update specific
+    // Update specific -> you can use Car.findById
     // const updateCar = await Car.updateOne({_id: '6251485f97bbd77777261a52'}, {
     //     name: 'Alphard'
     // })
-
     // console.log(updateCar);
+
+    // Delete one
+    // const deleteUser = await Car.deleteOne({_id: '6251474999500ff0c16be759'});
+    // console.log(deleteUser)
 }
 
 
